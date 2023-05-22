f8938acc93c5c7ccb32816c0838b1bb3237bf0f8
Riddhi'sChaoticWorld <130882317+Riddhiman2005@users.noreply.github.com>
2023-05-22 09:50:08 +0530

    Create Code.php

---
+++ Code.php
@@ -0,0 +1,287 @@
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Hello friends, finally I'm done with all my Tests and committments. So, from now onwards you can expect a regular weekly blogs that was  very popular as the Saturday Content.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>So, it takes time to make Habits. And thus, EQP has again planned to run a series "Missing Piece of the Puzzle of Universe".  This is the first of the series and every week you'll get </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":693,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/image-1-edited.png" alt="" class="wp-image-693"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>So, let's start.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>In this post, we will shine some light on the introduction of Dark Matter, its Background, the distinction between Dark Matter and Energy and finally understanding the first experimental detection of this Unknown Matter.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"large"} -->
+<p class="has-large-font-size"><strong><span style="text-decoration: underline">Introduction</span></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":687,"width":664,"height":373,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/image-3.png?w=600" alt="" class="wp-image-687" width="664" height="373"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Humans have an insatiable need to understand the world around them, from the origins of our own existence to the vastness of the universe. Yet, despite all the progress we've made, there are still mysteries that elude us. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>For centuries, scientists have looked up at the night sky and marveled at the luminous matter that seemed to make up our universe. However, in the past century, experiments and observations have revealed that there must be something else out there that we cannot see. The existence of this mysterious substance, known as dark matter, has been theorized to explain the discrepancies in our current understanding of the universe.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Despite the fact that we have yet to directly observe dark matter, we have compelling indirect evidence for its existence. The search for dark matter is not just about explaining the anomalies in our observations but has much wider implications. It has the potential to help us understand the origins of the universe and the ultimate fate of the cosmos. While the universe may be vast and seemingly endless, the enigma of dark matter has captured the attention of scientists and laypeople alike.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Dark matter is not just a puzzle to solve, but a gateway to unlocking some of the biggest questions in the field of Cosmology and astrophysics.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":686,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/image-2.png?w=1024" alt="" class="wp-image-686"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"fontSize":"large"} -->
+<p class="has-large-font-size"><strong><span style="text-decoration: underline">Background</span></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Everything we touch, smell, taste,eat or see is composed of the same sort of atoms as the thousands of billions of stars that make up the Milky Way Galaxy in which we live. This same sort of atoms make up the stars, dusts and gases that compose the hundreds of billions of other galaxies in our visible Universe, or also the the gas which lies in the intergalactic depths.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Despite the fact that everything we experience in our daily lives is made up of the same sort of atoms, it's important to remember that these atoms are just a fraction of the stuff that makes up the universe. In fact, if we were to add up all the matter and energy in the universe, the sum total of everything we can see, from the tiniest speck of dust to the largest galaxy, would only account for less than 5% of the total. The vast majority of the universe is composed of something entirely different from the matter that makes up our world.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>It's a humbling realization that the stuff that we're made of is such a small part of the universe as a whole. But it also highlights just how much we have yet to learn about the cosmos. The study of <strong>dark matter</strong> and <strong>dark energy</strong> is just one example of the ongoing quest to understand the mysteries of the universe. As we continue to explore and push the boundaries of what we know, it's exciting to imagine what other discoveries and revelations may be waiting for us in the vast, unexplored depths of the cosmos.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Despite its invisibility as dark matter doesn't emit light , we know that it exists because of the gravitational effects it has on other bodies in the universe, such as stars and galaxies.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Scientists have long been fascinated by the phenomenon of dark matter, which was first hypothesized to account for discrepancies between the observed mass of galaxies and the mass that could be accounted for by the visible matter they contained. Over time, more and more evidence has accumulated that supports the existence of dark matter, and it is now widely accepted as a fact of the universe.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>The most popular theory to explain dark matter is that it is composed of a new type of particle known as weakly interacting <strong>massive particles(WIMPs)</strong> [<em>We will discuss this article in details in our upcoming posts</em>] for short. These particles are thought to interact only through gravity and the weak force, which makes them incredibly difficult to detect or observe directly.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>While there are alternative theories that have been proposed to explain dark matter, there is still much that we don't know about this elusive substance</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":704,"width":462,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/image-6-edited.png" alt="" class="wp-image-704" width="462"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Now,  we'll move to next section . But before that let's understand Baryonic Matter. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"large"} -->
+<p class="has-large-font-size"><strong><span style="text-decoration: underline">BARYONIC MATTER</span></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":701,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/image-5.png?w=223" alt="" class="wp-image-701"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>A baryon is a particle made from <strong>three quarks, like a proton or a neutron</strong>. Electrons are not actually baryons - they are leptons - but because most of the mass of an atom is made up of the protons and neutrons, we still tend to call atoms "baryonic matter".</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"align":"center","id":696,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-large"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/image-4-edited.png" alt="" class="wp-image-696"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>Fermions are classified into two families:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":705,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/image-7.png?w=660" alt="" class="wp-image-705"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li><strong>Quarks</strong>: They  experience the strong force. They are always found in ‘bundles’ of <strong>2 (mesons)</strong> or <strong>3 (baryons)</strong>. Collectively, these&nbsp;quark&nbsp;bundles are referred to as hadrons.</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li><strong>Leptons</strong> which do not experience the strong force.</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p><strong>Baryons contain 3 quarks</strong>. There are many different baryons observed in particle accelerators (e.g. <strong>Λ<sup>0</sup>, Σ<sup>+</sup>, Σ<sup>–</sup>, Σ<sup>0</sup>, Δ<sup>+</sup>, Δ<sup>–</sup>, Δ<sup>0</sup>,</strong> ….), simply because there are many ways to make allowable combinations of 3 from the 12 quarks available (including anti-quarks)[<em>Maths students can try to find number of possible combinations</em>]. The vast majority of these extremely massive particles are highly unstable, decaying as a shower of smaller particles in as little as 10<sup>-23</sup>&nbsp;seconds. In nature, there are only 2 common baryons –&nbsp;<strong>protons</strong>&nbsp;and <strong>neutrons</strong> – and together they dominate the&nbsp;mass&nbsp;of normal matter in the Universe</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>The term is used loosely in the world of astronomy and Astrophysics. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>When we say "<strong>baryonic matter</strong>", we mean most of what we consider "ordinary" matter as opposed to dark matter, which is probably made up of a different kind of particle.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"large"} -->
+<p class="has-large-font-size"><strong><span style="text-decoration: underline">DARK MATTER VS DARK ENERGY</span></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Despite their similar names, they are two distinct phenomena with different properties and effects. In this article, we will explore the differences between dark matter and dark energy, with the help of diagrams.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Dark Matter:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Dark matter is a form of matter that does not interact with light or other forms of electromagnetic radiation, making it invisible to our telescopes. We know it exists because of its gravitational effects on visible matter. For example, the stars in a galaxy move faster than they should be, given the amount of visible matter in the galaxy. This suggests that there is more matter in the galaxy than we can see, and that matter is dark matter.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":707,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/image-8.png?w=440" alt="" class="wp-image-707"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>The image above shows the rotation curve of a galaxy. The blue line shows the observed velocity of stars in the galaxy, while the red line shows the predicted velocity based on the visible matter in the galaxy. The fact that the observed velocity is higher than the predicted velocity suggests that there is more matter in the galaxy than we can see, and that matter is dark matter.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Dark matter is thought to make up around 27% of the total mass-energy of the universe. It is believed to be composed of particles that are much heavier than electrons, but much lighter than atoms. These particles are collectively known as Weakly Interacting Massive Particles (WIMPs).</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Diagram 2:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>[Insert image of WIMPs interacting with visible matter]</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>The image above shows how WIMPs are thought to interact with visible matter. WIMPs do not interact with light or other forms of electromagnetic radiation, but they do interact with gravity and the weak nuclear force. This allows them to exert a gravitational force on visible matter, which is how their presence is detected.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Dark Energy:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Dark energy is a form of energy that is thought to be causing the universe to expand at an accelerating rate. This was first discovered in the late 1990s by studying distant supernovae. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Scientists expected to see the expansion of the universe slowing down over time, but instead they found that it was speeding up.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":710,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/image-11.png?w=960" alt="" class="wp-image-710"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>The image above shows how the universe is thought to be accelerating in its expansion. As time goes on, the galaxies in the universe are moving away from each other at an increasing rate.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Dark energy is believed to make up around 68% of the total mass-energy of the universe. Unlike dark matter, it is not a form of matter but a property of space itself. It is believed to be uniform throughout the universe, and its effects are seen on the largest scales.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":708,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/image-9.png?w=1024" alt="" class="wp-image-708"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>The image above shows the cosmic microwave background radiation, which is a faint glow of radiation that fills the entire universe. This radiation is thought to have been created just 380,000 years after the Big Bang, and it provides a snapshot of what the universe looked like at that time. Scientists have studied this radiation to learn more about the properties of dark energy and how it is affecting the universe.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"large"} -->
+<p class="has-large-font-size"><strong><span style="text-decoration: underline">FIRST DETECTION OF DARK MATTER</span></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":713,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/image-12.png?w=404" alt="" class="wp-image-713"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>The first detection of dark matter is attributed to Fritz Zwicky (1933), who<br>measured the velocity dispersion of galaxies in the Coma cluster and found<br>their velocities to far exceed that which could be attributed to the luminous<br>matter in the galaxies themselves. <br>He looked at velocity measurements of several individual galaxies in the<br>Coma cluster and noticed that the galaxies seemed to be moving too fast for<br>the amount of visible matter in the cluster. If one adds up the mass in the<br>cluster by assuming that every galaxy has a mass-to-light ratio of about one<br>in solar units, then there isn’t enough mass to hold the cluster together, i.e.,<br>the galaxies should fly apart.<br>Zwicky estimated the cluster's total mass based on the motions of galaxies<br>near its edge (using the virial theorem) and compared that estimate to one<br>based on the number of galaxies and total brightness of the cluster. He found<br>that there was about 400 times more estimated mass than was visually<br>observable. The gravity of the visible galaxies in the cluster would be far too<br>small for such fast orbits, so something extra was required. This is known as<br>the "missing mass problem". Based on these conclusions, Zwicky inferred<br>that there must be some non-visible form of matter which would provide<br>enough of the mass and gravity to hold the cluster together</p>
+<!-- /wp:paragraph -->
