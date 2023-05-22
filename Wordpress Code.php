212161107b0b8d6454c7cc9d5b783105e2dca9ac
Riddhi'sChaoticWorld <130882317+Riddhiman2005@users.noreply.github.com>
2023-05-22 16:43:08 +0530

    Update Wordpress Code.php

--- Wordpress Code.php
+++ Wordpress Code.php
@@ -1,1 +1,186 @@
 
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>So, Hi everyone. We're again back with an exciting discussion and an interesting experiment which my friend Nikhil did to answer a simple question of Nikhil's curios brother Aaryan.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>So, first of all let me introduce Nikhil and Aaryan first.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><em>Nikhil- An afficiando of Physics, Mathematics and computer science often found in his room doing coding or building some interesting projects for Everlasting Quest for Physics blogs or <a rel="noreferrer noopener" href="https://normalisedvector.wordpress.com/" target="_blank">Normalised Vectors</a>. Well, do check it obviously. It has lot of exciting Maths Stuffs and some Ins πing (Well readers, Decode the word and write in comments section) Experiences ?</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><em>Aaryan- With his unbridled curiosity and passion for physics and space, Aaryan, Nikhil's younger brother is sure to touch the sky one day. The "<strong>little astronaut's</strong>" thought provoking questions often keep Nikhil and his friends baffled.</em></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Aaryan just got to know about <strong>Plasma</strong> in his class. The 4th state of matter.  And he went to his brother to ask if they can perceive or see plasma the same way like solids, liquids and gases.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Aaryan: Brother, can't we experience Plasma in daily life. I got to know that Plasma has lot of applications in the field of welding, wound healing</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Nikhil: Yes, that's right. But experiencing Plasma in Home!!!, How it's hot, it's not a child's Play... It's ionised gas, How can we generate it... How...</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>... (Phone Ringing, Call from <strong>EQP </strong>)</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Nikhi: Yes Riddhi, Tell me. About blogs right.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Riddhi: Yes, I was planning for something interesting, you know. I mean if we can do some kind of intriguing experiments- Say Experiencing Plasma in Home.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Nikhil: What Happened to Earth today, You're asking the same question as my brother...</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Riddhi: (Smiling Hard), Bro, Got my topic. Why not Grape Plasma</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Nikhil: Grape Plasma!!! What's that??</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Riddhi: Ok, hear this and put it in speaker. Let Aaryan hear it. He'll have a good sleep if he is confirmed that we can experience Plasma in Our Home.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Nikhil: Ok. ( Aaryan, come here...)</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Riddhi: So, I'm talking about Microwave Induced Grape Plasma.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>So,  This phenomenon occurs when a pair of grape hemispheres are exposed to intense microwave radiation, causing them to spark and ignite a plasma emitting a variety of colorful and intriguing light spectra.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Nikhil: Wow Brother. How can we do that, and materials required...</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Wait, it was a call. And I should not tell everything discussed there. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>But we did the experiment. And you all can do it. It's very easy.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>So, the materials required are</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list {"ordered":true} -->
+<ol><!-- wp:list-item -->
+<li>A pack of Green Grapes. We may also use Hydrogel Water Beads to get the same results.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Microwave Oven</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Plates that can withstand Microwave vibrations and won’t crack</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Knife- For slicing the Grapes</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Camera or Mobile Phone to record the sparklings.</li>
+<!-- /wp:list-item --></ol>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>So, on the experiment day all of Nikhil's friend are ready on Goggle meet to see an outstanding experiment.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>And Riddhi is on call, for the Procedure and Nikhil to execute that. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>Slice the grape such that the thin isthmus is still holding the two halves together. Or one can also keep two grapes such that they are in touch</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:image {"id":666,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/slicing-the-grapes-with-isthmus-between.jpg?w=817" alt="" class="wp-image-666"/><figcaption class="wp-element-caption"><strong><em>Slicing the grapes leaving an isthmus</em></strong></figcaption></figure>
+<!-- /wp:image -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>Next, place them sliced side up. (only if you slice them)</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:image {"id":668,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/05/sliced-side-up.jpg?w=1024" alt="" class="wp-image-668"/><figcaption class="wp-element-caption">Grapes Sliced side up</figcaption></figure>
+<!-- /wp:image -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>Place the plate with the prepared grapes into the center of the microwave oven and shut the door carefully. The microwave is set to cook at full power for 35-40 seconds.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>The sparks will start approximately 5 seconds after the microwave is started.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Approximately 2-3 seconds after that, the force of the sparks separated the grape halves (the isthmus burns out) by approximately 1-1.5 cm, ending the effects. At this point, stop the microwave to prevent further damage</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"x-large"} -->
+<p class="has-x-large-font-size"><strong>RESULTS</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>We will see flares and sparks associated with the grapes as an effect of microwaves.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>But Aaryan forgot to turn on the camera. Oh shit, everything lost we</p>
+<!-- /wp:paragraph -->
